9eac9a6ab224f8e9040b1636e902d2fd30d55a7e
Filippov-Nikolay <filippovnikolay195@gmail.com>
2025-03-11 14:56:58 +0200

    hw-3

---
+++ lesson3/homework/homework/homework/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using homework.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace homework.Controllers
+{
+    public class MovieController : Controller
+    {
+        MoviesContext db;
+        public MovieController(MoviesContext context) {
+            db = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Movies> movies = await Task.Run(() => db.Movies);
+            ViewBag.Movies = movies;
+            return View();
+        }
+    }
+}
